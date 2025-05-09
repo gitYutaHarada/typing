c0f8fd048a01aafbcf91c48fbbea590d6a94776f
haradayuuta <mayutokuyuta@gmail.com>
2025-05-09 22:01:44 +0900

    ✨ 質問内容をコンポーネント化

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,6 @@ import { Question } from "@/compoments/question";
 export default function Home() {
   return (
     <div>
-      <h1>Hello</h1>
       <Question />
     </div>
   );
--- src/compoments/question/index.jsx
+++ src/compoments/question/index.jsx
@@ -1,11 +1,30 @@
+import classes from "@/compoments/question/question.module.css";
+
+const questions = [
+  "JavaScript",
+  "document",
+  "window",
+  "getElementById",
+  "getElementByClassName",
+  "addEventListener",
+];
+
+const entry = document.getElementById("entry");
+const remained = document.getElementById("remained");
+const inputText = document.getElementById("inputText");
+
+//フォームに値が入力された際
+document.addEventListener("input", (e) => {
+  console.log(e);
+});
 export const Question = () => {
   return (
-    <div>
-      <div class="question">
+    <div className={classes.textContainer}>
+      <div className={classes.question}>
         <span id="entry" className={classes.marker}></span>
         <span id="remained">JavaScript</span>
       </div>
-      <input type="text" className="text" id="inputText"></input>
+      <input type="text" className={classes.text} id="inputText"></input>
     </div>
   );
 };
